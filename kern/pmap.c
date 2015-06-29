c28c1091f6c0c65daba24f98b89a002baea105a1
F10 <mr.greyforever@gmail.com>
2015-06-29 16:36:01 +0800

    oh my

--- kern/pmap.c
+++ kern/pmap.c
@@ -638,7 +638,7 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 	void * end = ROUNDUP((void *)(va+len),PGSIZE);
 	for(;  va < end ; va= ROUNDDOWN(va+PGSIZE,PGSIZE)){
 		pte_t * pt_entry = pgdir_walk(env->env_pgdir,(const void *)va,0);
-		if((va >=(void *)ULIM) || !(((*pt_entry) & (perm || PTE_P)) == (perm || PTE_P ) ))
+		if( (pt_entry == NULL) || (va >=(void *)ULIM) || !(((*pt_entry) & (perm || PTE_P)) == (perm || PTE_P ) ))
 		{
 			user_mem_check_addr = (uintptr_t)va;
 			return -E_FAULT;
--- lib/pgfault.c
+++ lib/pgfault.c
@@ -27,9 +27,13 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 	int r;
 
 	if (_pgfault_handler == 0) {
+		int ret = 0;
+		if((ret = sys_page_alloc(thisenv->env_id,(void *)(UXSTACKTOP - PGSIZE),PTE_U|PTE_P| PTE_W)) < 0)
+			panic("set_pgfault_handler : %e",ret);
+		sys_env_set_pgfault_upcall(thisenv->env_id,_pgfault_upcall);
 		// First time through!
 		// LAB 4: Your code here.
-		panic("set_pgfault_handler not implemented");
+		//panic("set_pgfault_handler not implemented");
 	}
 
 	// Save handler pointer for assembly to call.
